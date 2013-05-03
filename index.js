86e2abf593a34a8dd05398a4200e496f393da544
Tim Cameron Ryan <id@timryan.org>
2013-05-03 16:01:42 -0400

    Scraping reworked

--- index.js
+++ index.js
@@ -4,76 +4,99 @@ var read = require('read');
 
 
 var api = scrapi({
-  "base": "https://sis.olin.edu/cgi-bin/student",
+  "base": "https://my.olin.edu/",
   "spec": {
-    "/cgi-bin/student/stugrds.cgi": {
-      "classes": {
-        "$query": "tr[align=center] tr.glbdatadark",
-        "$each": "(html) td:nth-child(1)"
+    "/ICS/": {
+      "loginform": {
+        "__VIEWSTATE": {
+          $query: "#__VIEWSTATE",
+          $value: "(attr value)"
+        },
+        "___BrowserRefresh": {
+          $query: "#___BrowserRefresh",
+          $value: "(attr value)"
+        }
+      }
+    },
+    "/ICS/My_StAR/My_Grades_and_Transcript.jnz": {
+      classes: {
+        $query: "#pg0_V_tblTermData tr",
+        $each: {
+          $query: "td",
+          $each: "(text)"
+        }
+      },
+      summary: {
+        $query: "#pg0_V_tblDivisonData tr",
+        $each: {
+          $query: "td",
+          $each: "(text)"
+        }
       }
     }
   }
 });
 
-function getGrades (sess, year, next) {
-  api('setopt.cgi').post({
-    prog:"UNDG",
-    sess: sess,
-    yr: year,
-    setopt_command: "Submit Options",
-    action: "https://sis.olin.edu/cgi-bin/student/stugrds.cgi"
-  }, function (err) {
-    console.log('Reading', sess, year);
-    api('stugrds.cgi').get(function (err, json, res) {
-      json.classes = (json.classes || []).map(function (c) {
-        var data = c.replace(/<br>/g, ' ').replace(/<\/td>/g, '').split('<td valign="top">');
-        return {
-          section: data[1],
-          name: data[2].replace(/^\s+|\s+$/g, ''),
-          credit: parseFloat(data[3]),
-          grade: data[4],
-          type: data[0].replace(/\d.*$/, ''),
-          number: data[0].replace(/^[a-z]+/i, ''),
-          id: data[0]
-        };
-      });
+function getCredits (username, password, next) {
+  api('/ICS/').get(function (err, json) {
+    api('/ICS/').post({
+      "__VIEWSTATE": json.loginform.__VIEWSTATE,
+      "___BrowserRefresh": json.loginform.___BrowserRefresh,
+      "userName": username,
+      "password": password,
+      "btnLogin": "Login"
+    }, function (err, html, res) {
+      if (res.statusCode != 302) {
+        throw new Error('Invalid login.');
+      }
 
-      next(err, json);
+      api('/ICS/My_StAR/My_Grades_and_Transcript.jnz').get({
+        portlet: "Unofficial_Transcript_OLIN"
+      }, function (err, json) {
+        if (err) {
+          return next(err);
+        }
+
+        var years = {}, cur = [];
+        json.classes.forEach(function (m) {
+          if (m[0].match(/^\d+/)) {
+            // 2009, 2010...
+            cur = years[m[0]] = [];
+          } else {
+            m = m.map(function (n) {
+              return (n.match(/<\/?td>/) ? '' : n).replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ');
+            })
+            if (m.length > 1 && m[1].match(/\-/)) {
+              // Class name
+              cur.push({
+                course: m[1],
+                title: m[2],
+                grade: m[3],
+                repeat: m[4],
+                attemptedcredits: m[5],
+                earnedcredits: m[6],
+                passcredits: m[7],
+                gpacredits: m[8],
+                qualitypoints: m[9],
+                nondegreecredits: m[10],
+                gpa: m[11] || '',
+                notes: m[12] || ''
+              });
+            } else {
+              // Totals
+            }
+          }
+        });
+        next(err, years);
+      })
     });
   });
 }
 
-read({prompt: 'SIS Username:'}, function (err, username) {
-  read({prompt: 'SIS Password:', silent: true}, function (err, password) {
-    api.pre('request', function (req, next) {
-      req.headers.authorization = 'Basic ' + new Buffer(username + ':' + password, 'utf-8').toString('base64');
-      next();
-    });
-
-    async.mapSeries([
-      ['FA', 2009],
-      ['SP', 2010],
-      ['FA', 2010],
-      ['SP', 2011],
-      ['FA', 2011],
-      ['SP', 2012],
-      ['FA', 2012],
-      ['SP', 2013]
-    ], function (arg, next) {
-      getGrades(arg[0], arg[1], next);
-    }, function (err, json) {
-      var classes = Array.prototype.concat.apply([], json.map(function (a) {
-        return a.classes;
-      }));
-      console.log(classes);
-
-      // Tally credits.
-      var credits = classes.reduce(function (last, c) {
-        last[c.type] || (last[c.type] = 0);
-        last[c.type] += c.credit;
-        return last;
-      }, {})
-      console.log('\ncredits:', credits);
-    });
+if (!module.parent) {
+  read({prompt: 'my.olin.edu Username:'}, function (err, username) {
+    read({prompt: 'my.olin.edu Password:', silent: true}, function (err, password) {
+      getCredits(username, password, console.log);
+    })
   })
-})
+}
